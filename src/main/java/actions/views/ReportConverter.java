fa05adea7bcc2ca52e1f4c2732e33dbde2d25a6a
上田　俊輔 <omba@localhost>
2023-09-04 17:48:02 +0900

    Reportモデル、ReportViewモデル作成

---
+++ src/main/java/actions/views/ReportConverter.java
@@ -0,0 +1,66 @@
+package actions.views;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Report;
+
+/**
+ * 日報データのDTOモデル⇔Viewモデルの変換を行うクラス
+ *
+ */
+public class ReportConverter {
+
+    /**
+     * ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
+     * @param rv ReportViewのインスタンス
+     * @return Reportのインスタンス
+     */
+    public static ReportView toView(Report r) {
+
+        if(r == null) {
+            return null;
+        }
+
+        return new ReportView(
+                r.getId(),
+                EmployeeConverter.toView(r.getEmployee()),
+                r.getReportDate(),
+                r.getTitle(),
+                r.getContent(),
+                r.getCreatedAt(),
+                r.getUpdatedAt());
+    }
+
+    /**
+     * DTOモデルのリストからViewモデルのリストを作成する
+     * @param list DTOモデルのリスト
+     * @return Viewモデルのリスト
+     */
+    public static List<ReportView> toViewList(List<Report> list) {
+        List<ReportView> evs = new ArrayList<>();
+
+        for (Report r : list) {
+            evs.add(toView(r));
+        }
+
+        return evs;
+    }
+
+    /**
+     * Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
+     * @param r DTOモデル(コピー先)
+     * @param rv Viewモデル(コピー元)
+     */
+    public static void copyViewToModel(Report r, ReportView rv) {
+        r.setId(rv.getId());
+        r.setEmployee(EmployeeConverter.toModel(rv.getEmployee()));
+        r.setReportDate(rv.getReportDate());
+        r.setTitle(rv.getTitle());
+        r.setContent(rv.getContent());
+        r.setCreatedAt(rv.getCreatedAt());
+        r.setUpdatedAt(rv.getUpdatedAt());
+
+    }
+
+}
